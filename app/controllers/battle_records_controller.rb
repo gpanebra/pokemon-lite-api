d48e7f196a1c3581e0c57ff5a97b8a2dcf573c4f
_hyphen <3930353-_hyphen@users.noreply.gitlab.com>
2020-05-21 16:11:35 -0500

    added main controllers

---
+++ app/controllers/battle_records_controller.rb
@@ -0,0 +1,17 @@
+class BattleRecordsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  # def create
+  # end
+
+  def update
+  end
+
+  # reset to 0
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :pokemons, :trainers
+  # TODO: add routes for battle records
 end
