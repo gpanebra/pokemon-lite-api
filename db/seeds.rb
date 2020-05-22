49ae39db4611cce4e9e3468a07a1799654c40ae1
adonai-luque <akluqueprieto@gmail.com>
2020-05-22 12:57:21 -0500

    adonai-luque

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+puts "Seeding begins"
+
+csv = CSV.read('db/pokemon-lite-api.csv', { headers: true, header_converters: :symbol })
+
+csv.each do |row|
+  
+  pokemon = Pokemon.find_or_create_by(
+    name: row[:name],
+    base_experience: row[:base_exp],
+    main_type: row[:main_type],
+    main_ability: row[main_ability]
+  )
+
+  trainer = Trainer.find_or_create_by(
+    name: row[:t_name],
+    gender: row[:t_gender],
+    age: 18,
+    home_region: row[:t_region],
+    team_member_status: row[:t_team_member],
+    wins: row[:wins],
+    losses: row[:losses]
+  )
+
+end
+
+puts "Seeding ends"
